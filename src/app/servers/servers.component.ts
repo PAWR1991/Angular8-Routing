1e285e2bd20d35ae5f03322a9df3f3f6b4db06f8
Paul <paul.roberts@C02XT4NCJGH5.fios-router.home>
2020-01-09 10:14:50 -0500

    Relative Paths in Programmatic Navigation

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-servers',
@@ -9,10 +10,20 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   private servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
+  }
+  /**
+   * router.navigate doesn't know the relative route too unlike routerLink
+   * routerLink knows where it is to the current url
+   */
+  onLoadServers(){
+    this.router.navigate(['servers'], {relativeTo: this.route});
   }
 
 }
