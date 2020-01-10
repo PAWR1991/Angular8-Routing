51ec765ae6dcf7b7b612c940a2b3f3f3e15689b0
Paul <paul.roberts@c02xt4ncjgh5.amer.reisystems.com>
2020-01-10 12:42:51 -0500

    Fetching Route Param Reactively

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -16,6 +16,13 @@ export class UserComponent implements OnInit {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     };
+    //param Obs
+    //When you are already in this component and make changes to the same url with different param data
+    //This is the safes way to make sure that angular refects the changes to the url to the DOM
+    this.route.params.subscribe((params:Params) => {
+      this.user.id = params['id'];
+      this.user.name = params['name'];
+    });
   }
 
 }
