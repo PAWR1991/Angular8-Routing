93a876701439a9b44864242f1d8a1e11151bfa7f
Paul <paul.roberts@c02xt4ncjgh5.amer.reisystems.com>
2020-01-13 10:18:34 -0500

    Using Query Parameters

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -11,8 +11,10 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService,
-    private route : ActivatedRoute) { }
+  constructor(
+    private serversService: ServersService,
+    private route : ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
     // the '+' convert a string into a number 
@@ -21,6 +23,10 @@ export class ServerComponent implements OnInit {
     this.route.params.subscribe((params:Params) => {
       this.server = this.serversService.getServer(+params['id']);
     });
+  }
+
+  onEdit(){
+    this.router.navigate(['edit'], {relativeTo: this.route});
   }
 
 }
